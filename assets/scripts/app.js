db895d5678392b5f7c92d03dcf8b493ef3b02490
DaisukeOda <hinopon25@gmail.com>
2017-06-17 13:50:22 +0900

    ファイルタイプ判定とかの機能追加

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,12 @@
-var app = angular.module('App', []);
+var app = angular.module('App', ['ui.bootstrap']);
 
 var url = {
 	items : './src/items.php',
 	upload : './src/upload.php'
+};
+
+var path = {
+	storage : 'storage/'
 };
 
 // ajaxリクエスト送るときの設定
@@ -22,9 +26,18 @@ app.config(function ($httpProvider) {
 
 });
 
-app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('MainCtrl', ['$scope', '$modal', '$http', function($scope, $modal, $http) {
 	$scope.file = '';
 	$scope.selected = [];
+
+	//現在選択中のファイルの情報が入る
+	$scope.currentFile = {
+		filename : '',
+		modified : 0,
+		created : 0,
+		ext : '',
+		size : 0
+	};
 
 	//ストレージからファイル一覧を取ってくる
 	(function() {
@@ -38,10 +51,11 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 		.then(function(res) {
 			$scope.items = res;
 		},function(res) {
-			console.log(res);
+			console.error(res);
 		});
 	})();
 
+	//ファイルアップロード
 	$scope.fileSubmit = function() {
 		var fd = new FormData();
 		fd.append('file', $scope.file);
@@ -53,21 +67,54 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 		})
 		.success(function(res) {
 			$scope.response = res;
-			console.log(res);
+
 			if(typeof res.file.error != undefined && res.file.error == 0) {
 				$scope.items.data.push([res.file.name]);
 			}
+
+		})
+		.error(function(res) {
+			console.error(res);
 		});
 	}
 
+
+	//選択中のアイテムのファイルタイプを判定。あとで外部関数にする
+	var showFilesEXT = {
+		image : ['.jpg', '.png', '.jpeg', '.gif', '.svg', '.bmp'],
+		video : ['.mov', '.mp4', '.avi', '.mpeg'],
+		sound : ['.mp3', '.wav', '.m4a', '.wma'],
+		adobe : ['.ai', '.art', '.psd', '.ae', '.xd']
+	};
+	function is_filetype(filename, filetype) {
+		if(showFilesEXT[filetype] == undefined) {
+			console.error('file type undefined : ' + filetype);
+			return false;
+		}
+		for (var i = 0; i < showFilesEXT[filetype].length; i++) {
+			if(filename.indexOf(showFilesEXT[filetype][i]) != -1)
+				return true;
+		}
+		return false;
+	}
+
+	//アイテムをモーダル表示
 	$scope.showItem = function($index) {
-		var target = $scope.items.data[$index];
 
 		// zipファイルじゃなかったら普通新規タブで表示
-		if(target.indexOf('.zip') == -1) {
-			window.open('http://localhost/angularDrive/storage/' + target);
+		if(is_image($scope.currentFile.filename) || is_video($scope.currentFile.filename)) {
+			$modal.open({
+				templateUrl : 'modalContentForm',
+				controller : 'ModalInstance',
+				scope: $scope
+			});
 
+			file_showable = false;
+
+		} else {
+			alert('Cant preview the file:' + $scope.currentFile.filename);
 		}
+
 	}
 
 	// クリックでアイテムを選択状態にする
@@ -80,6 +127,20 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 			}
 			$scope.selected[$index] = true;
 		}
+		var filename = $scope.items.data[$index]
+		var currentFileInfo = {
+			filename : filename,
+			src : path.storage + $scope.items.data[$index],
+			size : 0,
+			is_image : (is_filetype(filename, 'image')),
+			is_vide : (is_filetype(filename, 'video')),
+			is_adobe : (is_filetype(filename, 'adobe')),
+			is_unknown : (!(is_filetype(filename, 'image') ||
+				is_filetype(filename, 'video') ||
+				is_filetype(filename, 'adobe')))
+		};
+
+		$scope.currentFile = currentFileInfo;
 	}
 
 	$scope.deleteItem = function($index) {
@@ -104,6 +165,12 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
 	}
 
+}]);
+
+app.controller('ModalInstance', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+	$scope.closeModal = function() {
+		$modalInstance.close();
+	}
 }]);
 
 app.directive('fileModel', function($parse) {
--- index.php
+++ index.php
@@ -29,10 +29,11 @@
   <meta itemprop="image" content="https://sample.jp/sample.jpg">
   <meta name="description" content="サンプル説明文">
   <meta name="keywords" content="キーワード,キーワード2">
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min.js"></script>
   <script src="./assets/scripts/app.js"></script>
-  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="./d/styles/style.css">
   <title>AngularDrive</title>
@@ -41,8 +42,8 @@
   <div class="row">
     <section id="left" class="panel">
       <div class="inner">
-        <div class="white">
-          <h2 class="p-a">メニュー</h2>
+        <div class="green">
+          <h4 class="p-a text-white">メニュー</h4>
           <ul class="side-menu">
             <li><i class="ion-ios-time current icon"></i>最近のファイル</li>
             <li><i class="ion-star icon"></i>スター</li>
@@ -50,7 +51,7 @@
           </ul>
         </div>
         <div class="p-a">
-          <p class="text--help">11GB of 15GB used.</p>
+          <p class="text--help">45.4MB used.</p>
         </div>
       </div>
     </section>
@@ -62,7 +63,7 @@
           <button class="btn" ng-click="fileSubmit()">アップロード</button>
         </div>
         <div class="">
-          <h2 class="white p-a">Storage</h2>
+          <h2 class="green p-a text-white">Storage</h2>
           <ul class="files-list">
             <li ng-repeat="item in items.data" ng-class="{selected: selected[$index]}" ng-click="selectItem($index)" ng-dblclick="showItem($index)" ng-model="itemLI">
               <div class="flex-6">
@@ -79,12 +80,33 @@
       </div>
     </section>
     <section id="right" class="panel">
-      <div class="inner">
-        <div class="">
-          ファイル詳細を表示
-        </div>
+      <div class="inner p-a">
+        <h3>ファイル詳細</h3>
+        <p>{{currentFile.filename}}</p>
+        <p>Size : {{currentFile.size}}</p>
+        <p>Created : {{currentFile.size}}</p>
+        <p>Modified : {{currentFile.size}}</p>
+        <p>File Type :
+          <span ng-show="currentFile.is_image">Image</span>
+          <span ng-show="currentFile.is_video">Video</span>
+          <span ng-show="currentFile.is_adobe">Adobe</span>
+          <span ng-show="currentFile.is_unknown">unknown</span>
+        </p>
       </div>
     </section>
   </div>
+  <script type="text/ng-template" id="modalContentForm" class="modal-wrap">
+    <div id="modalContent" class="inner">
+      <div class="modal-header">
+        <h3>{{currentFile.filename}}</h3>
+      </div>
+      <div class="modal-body">
+        <img ng-show="currentFile.is_image" ng-src="{{currentFile.src}}">
+        <div class="modal-overlay" ng-click="closeModal()"></div>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </script>
 </body>
 </html>
